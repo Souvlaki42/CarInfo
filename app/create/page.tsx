dfcf6a244ee1952691785e8467411b967ccb3745
Souvlaki42 <souvlaki420@gmail.com>
2023-08-19 03:40:09 +0300

    Create page cleared

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,50 +1,7 @@
-import { prisma } from "@/lib/db";
-import { CarCard } from "@/components/car-card";
-import { SearchInput } from "@/components/search-input";
-
-export default async function IndexPage({
-  searchParams,
-}: {
-  searchParams: { search: string };
-}) {
-
-  const { search: searchQuery } = searchParams;
-  let cars;
-
-  if (searchQuery == null || searchQuery == undefined || searchQuery === "") {
-    cars = await prisma.car.findMany({
-      take: 10,
-      orderBy: { updatedAt: "desc" },
-    });
-  } else {
-    cars = await prisma.car.findMany({
-      take: 10,
-      orderBy: { updatedAt: "desc" },
-      where: {
-        OR: [
-          { frame: { contains: searchQuery } },
-          { engineNumber: { contains: searchQuery } },
-          { year: { contains: searchQuery } },
-        ],
-      },
-    });
-  }
-
+export default async function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <h1>Hello World!</h1>
-      <SearchInput />
-      <div className="flex flex-col gap-4">
-        {cars.map((car) => (
-          <CarCard
-            key={car.id}
-            id={car.id}
-            engineNumber={car.engineNumber}
-            frame={car.frame}
-            year={car.year}
-          />
-        ))}
-      </div>
     </section>
   );
 }
