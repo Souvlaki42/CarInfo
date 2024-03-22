f8d7d348f49491e251c7edbc45228f837febad4f
Souvlaki42 <77171255+Souvlaki42@users.noreply.github.com>
2024-03-23 01:47:10 +0200

    Header logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { db } from "@/db";
-import { getSession } from "@/lib/auth";
 
 export default async function HomePage() {
 	const vehicles = await db.query.vehicles.findMany();
-	const session = await getSession();
-	console.log(session);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			{vehicles.map((vehicle) => {
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,20 +11,40 @@ import {
 import { LogInIcon, LogOutIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
 
 const AccountDropdown = () => {
 	const session = useSession();
 	const isLoggedIn = !!session.data;
+
+	const getAvatarFallback = (name?: string | null) => {
+		const parts = name?.trim().split(" ");
+		let shortName = "";
+		if (!parts || parts.length < 1) shortName = "AV";
+		else if (parts.length === 1) shortName = parts[0].slice(0, 2);
+		else if (parts.length === 2)
+			shortName = parts[0].charAt(0) + parts[1].charAt(0);
+		else {
+			for (const part of parts) {
+				shortName += part.charAt(0);
+			}
+		}
+		return shortName.toUpperCase();
+	};
+
+	const name = session.data?.user.name;
+	const image = session.data?.user.image ?? "";
 
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Button variant={"ghost"}>
 					<Avatar className="mr-2">
-						<AvatarImage src={session.data?.user?.image ?? ""} />
-						<AvatarFallback>AV</AvatarFallback>
+						<AvatarImage src={image} />
+						<AvatarFallback>{getAvatarFallback(name)}</AvatarFallback>
 					</Avatar>
-					{session.data?.user?.name}
+					{name}
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent>
@@ -44,9 +64,20 @@ const AccountDropdown = () => {
 
 export const Header = () => {
 	return (
-		<header className="container mx-auto dark:bg-gray-900 py-4 bg-gray-100 rounded-b-lg">
+		<header className="container mx-auto dark:bg-gray-900 py-2 bg-gray-100 rounded-b-lg">
 			<div className="flex justify-between items-center">
-				<div>LOGO</div>
+				<Link
+					href={"/"}
+					className="flex gap-2 items-center text-xl hover:underline"
+				>
+					<Image
+						src={"/icon.png"}
+						width={60}
+						height={60}
+						alt="the application icon of a magic flying car"
+					/>
+					CarInfo
+				</Link>
 				<div className="flex items-center gap-4">
 					<AccountDropdown />
 					<ModeToggle />
