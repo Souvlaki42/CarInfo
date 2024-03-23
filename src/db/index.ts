daf1e4e81b70e7e021c36ccfbdd83259ad29628a
Souvlaki42 <77171255+Souvlaki42@users.noreply.github.com>
2024-03-23 13:56:43 +0200

    Prod dev dbs test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,12 @@
 import * as schema from "@/db/schema";
 import { env } from "@/lib/env";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle as drizzleDev } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { sql } from "@vercel/postgres";
+import { drizzle as drizzleProd } from "drizzle-orm/vercel-postgres";
 
 const queryClient = postgres(env.DATABASE_URL);
-const db = drizzle(queryClient, { schema });
+const dbDev = drizzleDev(queryClient, { schema });
+const dbProd = drizzleProd(sql, { schema });
 
-export { db };
+export const db = process.env.NODE_ENV === "development" ? dbDev : dbProd;
